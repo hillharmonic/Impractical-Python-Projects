9629491e16a7a86dee4a0b580944954036095eac
Andrew Hill <andrewh.psu@gmail.com>
2021-02-11 18:07:28 -0500

    2.11 update

--- Finding Palingrams/palindromes.py
+++ Finding Palingrams/palindromes.py
@@ -3,9 +3,18 @@ import load_dictionary
 word_list = load_dictionary.load('2of4brif.txt')
 pali_list = []
 
-for word in word_list:
-    if len(word) > 1 and word == word[::-1]:
-        pali_list.append(word) 
+def num_palindromes():
+    for word in word_list:
+        if len(word) > 1 and word == word[::-1]:
+            pali_list.append(word) 
 
-print("\nNumber of palindromes found = {}\n".format(len(pali_list)))
-print(*pali_list, sep='\n')
+    print("\nNumber of palindromes found = {}\n".format(len(pali_list)))
+    print(*pali_list, sep='\n')
+
+def is_palindrome(word):
+    if len(word) <2:
+        return True
+    if word[0] != word[-1]: 
+        return False
+    return is_palindrome(word[1:-1])
+            
